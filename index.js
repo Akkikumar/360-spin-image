11bfc63afc2a994f189ac59ffeeec1c25cfe50a9
Akkikumar <akkikumar625@gmail.com>
2021-11-30 12:11:48 +0530

    loop added

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const App = () => {
     'https://res.cloudinary.com/dewn0wy2s/image/upload/v1592668487/360/';
   const imgscrExt = '.png';
   const [range, setRange] = useState('1');
+  const [count, setCount] = useState(1);
   const [completeUrl, setCompleteUrl] = useState(imgsrc + range + imgscrExt);
   const rangeRef = useRef('0');
 
@@ -16,6 +17,19 @@ const App = () => {
     setRange(rangeRef.current.value);
     setCompleteUrl(imgsrc + range + imgscrExt);
   };
+
+  const loop = () => {
+    alert("1");
+    setCompleteUrl(imgsrc + count + imgscrExt);
+    setTimeout(
+      () => {        
+        if(count !== 10){
+          setCount(count => count + 1);
+          loop();
+        }
+      }, 1000
+    )    
+  }
 
   return (
     <div className="container">
@@ -29,8 +43,9 @@ const App = () => {
           min="1"
           max="42"
         />
-        <img src={completeUrl} />
+        <img src={completeUrl} />        
       </div>
+      <button onClick={loop}>play</button>
     </div>
   );
 };
