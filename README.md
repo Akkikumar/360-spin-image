bee7e4025a3d156010051dc5f24ff4dc1becad87
Akkikumar <akkikumar625@gmail.com>
2021-11-30 10:36:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-usestate-example-uxt1s6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-usestate-example-uxt1s6)
---
+++ index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState, useRef } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+const App = () => {
+  const imgsrc =
+    'https://res.cloudinary.com/dewn0wy2s/image/upload/v1592668487/360/';
+  const imgscrExt = '.png';
+  const [range, setRange] = useState('1');
+  const [completeUrl, setCompleteUrl] = useState(imgsrc + range + imgscrExt);
+  const rangeRef = useRef('0');
+
+  const work = () => {
+    console.log(rangeRef.current.value);
+    setRange(document.getElementById('range').value);
+    setCompleteUrl(imgsrc + range + imgscrExt);
+  };
+
+  return (
+    <div className="container">
+      <div id="main">
+        <input
+          type="range"
+          onChange={work}
+          ref={rangeRef}
+          id="range"
+          value="1"
+          min="1"
+          max="24"
+        />
+        <img src={completeUrl} />
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
